c582e6a64ced42e0a56009f20a954ae2d158a5ca
Gilberto Bernal <contact@clouds-news.com>
2016-09-21 21:54:05 -0400

    Sept. 21, 2016
    Includes BloodPresureRecord layout

--- BloodPressureRecord/app/src/main/java/com/bernal/gilberto/bloodpressurerecord/DataEnterActivity.java
+++ BloodPressureRecord/app/src/main/java/com/bernal/gilberto/bloodpressurerecord/DataEnterActivity.java
@@ -3,12 +3,11 @@ package com.bernal.gilberto.bloodpressurerecord;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DataEnterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.table_layout_colors);
+        setContentView(R.layout.data_enter_activity);
     }
 }
